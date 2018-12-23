e8fd6de4e91987a4927cab17f0ff94ee278ccf45
YAP CHENG WEI <yapchengwei@gmail.com>
2018-12-23 19:25:45 +0800

    update

--- app/src/main/java/com/ycw/blu/instagramclone/UsersTab.java
+++ app/src/main/java/com/ycw/blu/instagramclone/UsersTab.java
@@ -14,12 +14,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 
 /**
@@ -49,6 +54,7 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
         userTabLoadingTxt = view.findViewById(R.id.userTabLoadingTxt);
 
         userTabListView.setOnItemClickListener(UsersTab.this);
+        userTabListView.setOnItemLongClickListener(UsersTab.this);
 
         ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
         parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
@@ -82,6 +88,41 @@ public class UsersTab extends Fragment implements AdapterView.OnItemClickListene
 
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        return false;
+        ParseQuery <ParseUser> friend = ParseUser.getQuery();
+        friend.whereEqualTo("username", arrayList.get(position));
+        friend.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null && e == null) {
+//                    FancyToast.makeText(getContext(),
+//                            user.get("profileProfession") + "",
+//                            FancyToast.LENGTH_LONG,
+//                            FancyToast.SUCCESS,
+//                            false
+//                            ).show();
+                    final PrettyDialog userProfileDialog = new PrettyDialog(getContext());
+                    userProfileDialog.setTitle(user.getUsername()+"'s Info")
+                            .setMessage(
+                                    user.get("profileName")+"\n"+
+                                            user.get("profileBio")+"\n"+
+                                            user.get("profileProfession")+"\n"+
+                                            user.get("profileHobbies")+"\n"+
+                                            user.get("profileSport")+"\n"
+                            )
+                            .setIcon(R.drawable.ic_person_black_24dp)
+                            .addButton("OK",
+                                    R.color.pdlg_color_white,
+                                    R.color.pdlg_color_green,
+                                    new PrettyDialogCallback() {
+                                        @Override
+                                        public void onClick() {
+                                            userProfileDialog.dismiss();
+                                        }
+                                    })
+                            .show();
+                }
+            }
+        });
+        return true;
     }
 }
